29f4eca6156b0ab4e0193900fb54e2307498d559
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-01-24 23:55:10 -0400

    implement Redux

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
 import { AppRouter } from './routers/AppRouter';
+import { store } from './store/store';
 
 export const JournalApp = () => {
     return (
-        <div>
+        <Provider store={store}>
             <AppRouter />
-        </div>
+        </Provider>
     )
 }
