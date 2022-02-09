421dccecbaabcacb71d2e74ee49b22b811b3cb02
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-02-09 12:30:25 -0400

    actualiza nota actual seleccionada

--- src/store/actions/notes.js
+++ src/store/actions/notes.js
@@ -2,6 +2,7 @@ import {
     db,
     doc,
     setDoc,
+    updateDoc,
     collection,
 } from "../../firebase/firebase-config";
 import { loadNotes } from "../../helpers/loadNotes";
@@ -50,3 +51,22 @@ export const setNotes = (notes) => ({
     type: types.NOTES_LOAD,
     payload: notes,
 })
+
+
+//async action actualiza los datos a firestore
+export const startSaveNote = (note) => {
+    return async (dispatch, getState) => {
+        // const uid  = getState().auth.uid
+        const { uid } = getState().auth
+        //si note es undefined no se puede borrar el id por lo que verifica si existe
+        if (!note.url) {
+            delete note.url
+        }
+        //elimina el id por que no lo quiero guardar en la db
+        const noteToFirestore = { ...note }
+        delete (noteToFirestore.id)
+        const noteRef = doc(db, `${uid}/journal/notes/${note.id}`)
+        //actualiza en la referencia(noteref) con noteToFirestore(sin id)
+        await updateDoc(noteRef, noteToFirestore)
+    }
+}
